9531ca448d297bbdfa48cb5acf5ab6b586ed3303
sanjeevkupan18 <sanjeevkupan18@gmail.com>
2025-11-14 12:15:17 +0530

    Dynamic Routing

--- ReactRouterDom/src/main.jsx
+++ ReactRouterDom/src/main.jsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
+import { StrictMode ,useEffect,useState} from 'react'
 import { createRoot } from 'react-dom/client'
-import {Routes , Route,Link} from 'react-router-dom'
-import './index.css'
+import {Routes , Route,Link, useParams,NavLink} from 'react-router-dom'
+import './App.css'
 // import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 
 const Home=()=>{
-  return <div>
-    <h1>Home Page</h1>
+    const [post,setPost] = useState([])
+    useEffect(()=>{
+      fetch("https://jsonplaceholder.typicode.com/posts").then((data)=> data.json())
+      .then((data) => setPost(data));
+    },[])
+  return <div className='post-container'>
+    {
+      post.map((post)=>(
+        <NavLink className="post-title" style={{display:"block"}} to={`/post/${post.id}`}>{post.title}</NavLink>
+      ))
+    }
   </div>
 }
 const About=()=>{
@@ -25,6 +34,28 @@ const Setting=()=>{
     <h1>Setting Page</h1>
   </div>
 }
+const SayUser=()=>{
+  const params = useParams();
+  return <div>
+    <h1>My name is {params.userName}</h1>
+  </div>
+}
+
+const PostPage =()=>{
+  const params = useParams()
+  const [data ,setData]=useState(null)
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+      .then((data) => data.json())
+      .then((data) => setData(data));
+  },[])
+
+  if(data===null) return <p>Loading....</p>
+  return <div>
+    <h1>{data.title}</h1>
+    <p>{data.body}</p>
+  </div>
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -32,7 +63,10 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        // nested routing
+
+        <Route path="/user/:userName" element={<SayUser />} />
+        <Route path="/post/:postId" element={<PostPage />}  />
+        
         <Route path="account">
           <Route path="profile" element={<Profile />} />
           <Route path="setting" element={<Setting />} />
